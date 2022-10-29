c52ef532f85c15e0650e29d04d4ec08efcfb6264
Sergey Nikitin <niktiser@gmail.com>
2022-10-29 23:18:04 +0600

    to package

--- lib/pos_print_manager.dart
+++ lib/pos_print_manager.dart
@@ -1,14 +1,17 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+library pos_print_manager;
 
-import 'pos_print_manager_platform_interface.dart';
+import 'package:easy_logger/easy_logger.dart';
 
-class PosPrintManager {
-  Future<String?> getPlatformVersion() {
-    return PosPrintManagerPlatform.instance.getPlatformVersion();
-  }
+class PosPrinterManager {
+  static EasyLogger logger = EasyLogger(
+    name: 'pos_printer_manager',
+    defaultLevel: LevelMessages.debug,
+    enableBuildModes: [BuildMode.debug, BuildMode.profile, BuildMode.release],
+    enableLevels: [
+      LevelMessages.debug,
+      LevelMessages.info,
+      LevelMessages.error,
+      LevelMessages.warning
+    ],
+  );
 }
--- test/pos_print_manager_test.dart
+++ test/pos_print_manager_test.dart
@@ -1,29 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:pos_print_manager/pos_print_manager.dart';
-import 'package:pos_print_manager/pos_print_manager_platform_interface.dart';
-import 'package:pos_print_manager/pos_print_manager_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockPosPrintManagerPlatform
-    with MockPlatformInterfaceMixin
-    implements PosPrintManagerPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
 
 void main() {
-  final PosPrintManagerPlatform initialPlatform = PosPrintManagerPlatform.instance;
-
-  test('$MethodChannelPosPrintManager is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelPosPrintManager>());
-  });
-
-  test('getPlatformVersion', () async {
-    PosPrintManager posPrintManagerPlugin = PosPrintManager();
-    MockPosPrintManagerPlatform fakePlatform = MockPosPrintManagerPlatform();
-    PosPrintManagerPlatform.instance = fakePlatform;
-
-    expect(await posPrintManagerPlugin.getPlatformVersion(), '42');
+  test('adds one to input values', () {
+   
   });
 }
